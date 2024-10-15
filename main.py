01be129a860a1d1c7a26c4e8896d32d1e0f172ba
gusah90 <gusah0507@gmail.com>
2024-10-15 15:38:09 +0900

    Add btn2(clear) and handler func

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # ch 4.2.3 main.py
 import sys
-from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout,
+from PyQt5.QtWidgets import (QApplication,QWidget,QPushButton,QVBoxLayout, QHBoxLayout,
  QMessageBox,QPlainTextEdit)
 from PyQt5.QtGui import QIcon
 class Calculator(QWidget):
@@ -16,11 +16,21 @@ class Calculator(QWidget):
 
         self.btn1=QPushButton('Message',self)
         self.btn1.clicked.connect(self.activateMessage)
+        self.btn2=QPushButton('Clear',self)
+        self.btn2.clicked.connect(self.clearMessage)
 
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.btn1)
+        hbox.addWidget(self.btn2)
+        
+        
         vbox=QVBoxLayout()
         #vbox.addStretch(3)
         vbox.addWidget(self.te1)
-        vbox.addWidget(self.btn1)
+        #vbox.addWidget(self.btn1)
+        vbox.addLayout(hbox)
         vbox.addStretch(1)
         
         self.setLayout(vbox)
@@ -33,7 +43,8 @@ class Calculator(QWidget):
     def activateMessage(self):
         #QMessageBox.information(self,"information","Button clicked!")
         self.te1.appendPlainText("ButtonClicked")
- 
+    def clearMessage(self):
+        self.te1.clear()
 if __name__ =='__main__':
     app=QApplication(sys.argv)
     view=Calculator()
